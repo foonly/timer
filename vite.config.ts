577225a713ba776b787fda335b6f76a5a18f597f
Niklas Schönberg <niklas@uplink.fi>
2024-03-14 23:42:47 +0200

    PWA

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,29 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import svgLoader from "vite-svg-loader";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), svgLoader()],
+  plugins: [
+    vue(),
+    svgLoader(),
+    VitePWA({
+      registerType: "autoUpdate",
+      includeAssets: ["src/assets/hourglass.svg"],
+      manifest: {
+        name: "Foonlys Hierarchical Timer",
+        short_name: "FHT",
+        description: "Foonlys Hierarchical Timer",
+        theme_color: "#000000",
+        icons: [
+          {
+            src: "src/assets/hourglass.svg",
+            sizes: "all",
+            type: "image/svg",
+          },
+        ],
+      },
+    }),
+  ],
 });
